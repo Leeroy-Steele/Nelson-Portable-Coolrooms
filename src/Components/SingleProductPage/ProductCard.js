0f09b629d0b845f036c7dbfbb4225d4c3db6e903
Leeroy-Steele <leeroysteele@hotmail.com>
2023-10-08 12:07:35 +1300

    Products tabs working

--- src/Components/SingleProductPage/ProductCard.js
+++ src/Components/SingleProductPage/ProductCard.js
@@ -2,47 +2,42 @@ import React, { useState } from "react";
 import { HashLink as Link } from "react-router-hash-link"; // for enquiry button
 import ProductData from "../ProductData";
 import { useSearchParams, useNavigate } from "react-router-dom";
-
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 // for bootstrap
 import Card from "react-bootstrap/Card";
 
 export default function ProductCard() {
   const [searchParams] = useSearchParams();
-  const [productTitle, setProductTitle] = useState(searchParams.get("productTitle"))
+  const [productTitle] = useState(searchParams.get("productTitle"));
+  const [product, setProduct] = useState(
+    ProductData.filter((item) => item.title === productTitle)
+  );
 
-  const [product, setProduct] = useState(ProductData.filter((item)=>item.title===productTitle))
-  const[foo, setFoo] = useState(false)
-
+  // Change product when product button is pressed
   const navigate = useNavigate();
-  const HandleProductTitleTabs = (k)=>{
-    navigate(`/product?${k}`)
-    setFoo(!foo)
-  }
+  const HandleProductTitleTabs = (k) => {
+    navigate(`/product?${k}`);
+    setProduct(ProductData.filter((item) => item.title === k));
+  };
 
   // enquiry button path to navigate to (use query params to part fill enquiry form)
   let enquiryPath = `/Contact?enquirySubject=${productTitle}#enquiryAnchorTag`;
 
   return (
     <div className="container">
-
-    <Tabs
-      defaultActiveKey={productTitle}
-      id="fill-tab-example"
-      className="mb-3"
-      // fill
-      justify
-      onSelect={(k)=>HandleProductTitleTabs(k)}
-    >
-    {ProductData.map((item)=>
-      <Tab eventKey={item.title} title={item.title}></Tab>
-    )}
-      
-
-    </Tabs>
-
+      <Tabs
+        defaultActiveKey={productTitle}
+        id="fill-tab-example"
+        className="mb-3 d-md-flex d-none"
+        justify
+        onSelect={(k) => HandleProductTitleTabs(k)}
+      >
+        {ProductData.map((item) => (
+          <Tab eventKey={item.title} title={item.title}></Tab>
+        ))}
+      </Tabs>
 
       <div className="row row-cols-1 row-cols-md-2 justify-content-center pt-3 pb-5 mb-5">
         <Card className="border-0 pt-3 pb-5">
@@ -53,7 +48,7 @@ export default function ProductCard() {
               {product[0].subTitle}
             </Card.Subtitle>
 
-            <Link className="btn btn-primary w-25" to={enquiryPath}>
+            <Link className="btn btn-primary w-25 my-3" to={enquiryPath}>
               Enquire
             </Link>
 
